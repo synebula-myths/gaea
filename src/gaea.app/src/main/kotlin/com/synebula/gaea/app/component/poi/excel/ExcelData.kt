21434d8cd3812f63226a5fd4991442da1f4a6d44
alex <reizero@live.com>
2020-07-19 14:39:51 +0800

    增加excel导出通用组件

---
+++ src/gaea.app/src/main/kotlin/com/synebula/gaea/app/component/poi/excel/ExcelData.kt
@@ -0,0 +1,5 @@
+package com.synebula.gaea.app.component.poi.excel
+
+class ExcelData(var title: String = "",
+                var columnNames: List<String> = listOf(),
+                var data: List<List<String>> = listOf())
