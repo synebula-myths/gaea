0b79b63b2822e19dbc3f1ffa76d76c69431ba60c
alex <reizero@live.com>
2021-03-19 14:11:40 +0800

    修改代码格式等；调整分页参如兼容@排序字段

--- src/gaea/src/main/kotlin/com/synebula/gaea/query/Params.kt
+++ src/gaea/src/main/kotlin/com/synebula/gaea/query/Params.kt
@@ -11,36 +11,77 @@ import com.synebula.gaea.query.type.Order
  */
 data class Params(var page: Int = 1, var size: Int = 10) {
 
+    private var _parameters = mutableMapOf<String, Any>()
+    private var _orders = mutableMapOf<String, Order>()
+
     /**
      * 数据索引，从0开始。表示数据在总量的第几条。（index = (page - 1) * size）
      */
-    var index: Long = 0
-        get() = (page.toLong() - 1) * size.toLong()
+    var index: Int = 0
+        get() = (page - 1) * size
         private set
 
     /**
      * 排序条件。
      */
-    var orders: MutableMap<String, Order> = hashMapOf()
+    var orders: Map<String, Order>
+        set(value) {
+            this._orders = value.toMutableMap()
+        }
+        get() {
+            if (this._parameters.keys.count { it.startsWith("@") } > 0) {
+                val params = mutableMapOf<String, Any>()
+                this._parameters.forEach {
+                    if (it.key.startsWith("@")) {
+                        this._orders[it.key.removePrefix("@")] = Order.valueOf(it.value.toString())
+                    } else
+                        params[it.key] = it.value
+                }
+                this._parameters = params
+            }
+            return this._orders
+        }
 
     /**
      * 查询条件。
      */
-    var parameters: MutableMap<String, Any> = hashMapOf()
+    var parameters: Map<String, Any>
+        set(value) {
+            this._parameters = value.toMutableMap()
+        }
+        get() {
+            if (this._parameters.keys.count { it.startsWith("@") } > 0) {
+                val params = mutableMapOf<String, Any>()
+                this._parameters.forEach {
+                    if (it.key.startsWith("@")) {
+                        this._orders[it.key.removePrefix("@")] = Order.valueOf(it.value.toString())
+                    } else
+                        params[it.key] = it.value
+                }
+                this._parameters = params
+            }
+            return this._parameters
+        }
+
+    constructor(page: Int, size: Int, parameters: LinkedHashMap<String, Any>) : this(page, size) {
+        this.page = page
+        this.size = size
+        this._parameters = parameters
+    }
 
     /**
      * 添加查询条件
      */
-    fun addParameter(field: String, value: Any): Params {
-        parameters[field] = value
+    fun where(field: String, value: Any): Params {
+        _parameters[field] = value
         return this
     }
 
     /**
      * 添加排序条件
      */
-    fun addOrderBy(field: String, type: Order = Order.ASC): Params {
-        orders[field] = type
+    fun order(field: String, order: Order = Order.ASC): Params {
+        _orders[field] = order
         return this
     }
 }
